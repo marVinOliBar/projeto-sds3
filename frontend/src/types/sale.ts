1ba60518e098020ac51c65dd7ef6d91b245cf92a
Marcus Vinicius Barbosa <barbosa.mvo@gmail.com>
2021-11-06 12:12:18 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,26 @@
-import { type } from "os"
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    first: boolean;
+    size?: number;
+    number: number;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
 
 export type SaleSum = {
     sellerName: string;
